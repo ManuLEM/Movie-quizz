929bbb02167860dbc7be807be2bec11b1fc9f627
ManuLEM <manuel.lemaire@gmail.com>
2016-01-15 11:29:14 +0100

    Preparation for templating

--- scripts/views/home.js
+++ scripts/views/home.js
@@ -3,7 +3,14 @@ var HomeView = Backbone.View.extend({
   events: {
   },
   initialize: function() {
+    this.render();
   },
   render: function() {
+    var that = this;
+    $.get('scripts/templates/homeTemplate.hbs', function(data) {
+      var template = Handlebars.compile( data );
+
+      that.$el.html( template({test:'coucou'}) );
+    });
   }
 });
