e64c65bf8ccf4ff022bc6605944f908177efd59d
ManuLEM <manuel.lemaire@gmail.com>
2016-01-15 12:45:15 +0100

    Start home templating

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,12 +1,16 @@
 var Router = Backbone.Router.extend({
   routes: {
     '': 'showHome',
-    'play': 'showGame'
+    'play': 'showGame',
+    'highscores': 'showHighscores'
   },
   showHome: function() {
     this.view = new HomeView();
   },
   showGame: function() {
     console.log('play');
+  },
+  showHighscores: function() {
+    console.log('highscores');
   }
 });
