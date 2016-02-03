b5710012a24f8453b9d5ec82c8196538baf16a68
ManuLEM <manuel.lemaire@gmail.com>
2016-02-03 23:40:02 +0100

    Add api url

--- scripts/collections/question-collection.js
+++ scripts/collections/question-collection.js
@@ -1,4 +1,6 @@
 var QuestionCollection = Backbone.Collection.extend({
   model: QuestionModel,
-  url: 'mock/questions.json'
+  url: 'http://dcamilleri.com/wsf/api'
+  // used to test locally
+  // url: 'mock/questions.json'
 });
